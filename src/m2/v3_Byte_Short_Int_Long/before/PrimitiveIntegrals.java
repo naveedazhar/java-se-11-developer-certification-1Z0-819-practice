4a7e2b5de732fba0de205e4d16ae67ae9ce7ebf8
Naveed Azhar <naveed.azhar@3tsoftwarelabs.com>
2023-05-07 10:20:24 +0200

    first commit

---
+++ src/m2/v3_Byte_Short_Int_Long/before/PrimitiveIntegrals.java
@@ -0,0 +1,16 @@
+package m2.v3_Byte_Short_Int_Long.before;
+import static java.lang.System.out;
+
+public class PrimitiveIntegrals {
+
+    public static void main(String[] args) {
+        out.println("\n** Primitive Integrals ** \n");
+
+//        byte minByte = -129;
+//        byte maxByte = 127;
+//
+//        out.println(minByte);
+
+
+    }
+}
